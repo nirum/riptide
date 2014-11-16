584b021a8dd4c882632c9d6ad6317153a6d6be89
Niru Maheswaranathan <nirum@stanford.edu>
2014-11-15 19:02:20 -0800

    trying metrics-graphics instead of chartjs

--- public/js/app.js
+++ public/js/app.js
@@ -4,8 +4,8 @@
 console.log('hello!');
 
 // get the chart object
-var c = new Chart(document.getElementById("container").getContext("2d"));
-var colors = d3.scale.category10().domain([0,1,2,3,4,5]);
+//var c = new Chart(document.getElementById("container").getContext("2d"));
+//var colors = d3.scale.category10().domain([0,1,2,3,4,5]);
 
 // get form data
 expt = document.getElementById("name").innerHTML;
@@ -20,48 +20,30 @@ var dataplots = new Array(cols.length);
 _.each(cols, function (value, idx) {
     dataplots[idx] = {
         label: value.name,
-        active: value.checked
+        desc: "description goes here"
     }
 });
 
 d3.selectAll(".dataselect").on("change", function() {
-    dataplots[parseInt(this.value)].active = this.checked;
-});
+    var idx = parseInt(this.value);
 
-d3.select("#update").on("click", function () {
     d3.json('/api/' + expt, function (data) {
 
-        var start = moment(data[0].date);
-        var rad = parseInt(getval("#radius"));
-        var bezier = d3.select("#bezier")[0][0].checked;
-
-        var lineoptions = {
-            datasetFill: false,
-            pointDotRadius : rad,
-            bezierCurve : bezier
-        };
-
-        // the line data object for chartjs
-        var linedata = {
-            labels: _.map(data, function (d) {
-                return moment(d.date).diff(start, 'minutes');
-            }),
-            datasets: []
-        };
-
-        // loop
-        _.each(dataplots, function (metadata, colIdx) {
-            if (metadata.active) {
-                linedata.datasets.push({
-                    label: metadata.label,
-                    strokeColor: colors(colIdx),
-                    pointColor: colors(colIdx),
-                    data: _.map(data, function (d) {
-                        return d.data[colIdx];
-                    })
-                })
-            }
+        foo = _.map(data, function(d) {
+            return { time: moment(d.date).second(), value: d.data[idx] };
         });
-        c.Line(linedata, lineoptions);
+
+        data_graphic({
+            title: dataplots[idx].label,
+            description: dataplots[idx].desc,
+            data: foo,
+            area: false,
+            interpolate: 'linear',
+            width: 600,
+            height: 250,
+            target: '#container',
+            x_accessor: 'time',
+            y_accessor: 'value'
+        });
     });
 });
